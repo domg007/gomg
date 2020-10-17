bf7a54a1c48b3368df14853fee45c8fecedbbe5d
domg007 <33490244+domg007@users.noreply.github.com>
2020-10-17 22:29:37 +0800

    Create 虎牙自动发弹幕.user.js

---
+++ 虎牙自动发弹幕.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         虎牙自动发弹幕
+// @namespace    https://item.taobao.com/item.htm?spm=a2oq0.12575281.0.0.6aaf1debJ6Iafr&ft=t&id=605165564935
+// @note         可以自定义弹幕、自定义发送频率、随机发送弹幕
+// @description  虎牙自动发弹幕【淘宝店铺（因稀有才珍贵）制作】1.7.1025
+// @version      1.7.1025.02
+// @author       淘宝店铺（因稀有才珍贵）制作
+// @match        https://www.huya.com/*
+// @icon         https://www.huya.com/favicon.ico
+// @require      https://cdn.jsdelivr.net/combine/gh/domg007/gomg@V1.0.0926.01/AutoSend_Huya6.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_notification
+// ==/UserScript==
+(function() {
+    AutoSend_Huya("9e88e1a1a27596828b765db7d666ee1c");
+    
+
+})();
