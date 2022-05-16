f246b36fcb36517560d8bd6d9500daa8d7ef4fe2
domg007 <33490244+domg007@users.noreply.github.com>
2022-05-16 17:42:49 +0800

    Update AutoSend_Huya2.user.js

--- AutoSend_Huya2.user.js
+++ AutoSend_Huya2.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         虎牙自动发弹幕
-// @namespace    https://item.taobao.com/item.htm?spm=a2oq0.12575281.0.0.6aaf1debJ6Iafr&ft=t&id=605165564935
+// @namespace    https://item.taobao.com/item.htm?id=670749376549
 // @note         可以自定义弹幕、自定义发送频率、随机发送弹幕
-// @description  虎牙自动发弹幕【淘宝店铺（因稀有才珍贵）制作】1.9.215.2（增加休息时间）
-// @version      1.9.215.02
+// @description  虎牙自动发弹幕【淘宝店铺（因稀有才珍贵）制作】1.10.618（增加休息时间）
+// @version      1.10.618.01
 // @author       淘宝店铺（因稀有才珍贵）制作
 // @match        https://www.huya.com/*
 // @icon         https://www.huya.com/favicon.ico
-// @require      https://cdn.staticaly.com/gh/domg007/gomg@master/AutoSend_Huya7.js
+// @require      https://raw.githubusercontents.com/domg007/gomg/master/AutoSend_Huya7.js
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_notification
@@ -15,7 +15,7 @@
 // @license      因稀有才珍贵
 // ==/UserScript==
 (function() {
-    !!$("#J_roomTitle").length && AutoSend_Huya("b9e9a14e3f5a401bb0b135bb0dd8db62");
+    !!$("#J_roomTitle").length && AutoSend_Huya("def664be8d6978bb48c6033bc437365c");
     
 
 })();
