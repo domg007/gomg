d5aee2c13dd06a5df0ad445ed5b8874a827dec3d
domg007 <33490244+domg007@users.noreply.github.com>
2023-03-19 14:36:33 +0800

    Create xxx.user.js

---
+++ xxx.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         虎牙发弹幕无限制
+// @namespace    https://greasyfork.org/
+// @version      1.0.0320.2020
+// @description  无限制
+// @author       gomg
+// @match        *://www.huya.com/*
+// @icon         https://huyaimg.msstatic.com/cdnimage/actprop/20279_1__45_1532856204.jpg
+// @grant        none
+// @require      https://cdn.jsdelivr.net/combine/gh/domg007/gomg@V1.2.0319.1431/RemoveTimeLimit_Huya.js
+// @license      因稀有才珍贵
+// ==/UserScript==
+(function() {
+    'use strict';
+    huya_limit("xxx");
+})();
