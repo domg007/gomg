acacc863ce6e6d51d138b31fae3fe0f63aa3a43c
domg007 <33490244+domg007@users.noreply.github.com>
2025-06-06 12:48:07 +0800

    Create RemoveTimeLimit_Huya.user.js

---
+++ RemoveTimeLimit_Huya.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         虎牙发弹幕无限制
+// @namespace    https://greasyfork.org/
+// @version      1.0.0607.1700
+// @description  无限制
+// @author       gomg
+// @match        *://www.huya.com/*
+// @icon         https://huyaimg.msstatic.com/cdnimage/actprop/20279_1__45_1532856204.jpg
+// @grant        none
+// @require      https://fastly.jsdelivr.net/combine/gh/domg007/gomg@V1.2.0606.02/RemoveTimeLimit_Huya.js
+// @license      因稀有才珍贵
+// ==/UserScript==
+(function() {
+    'use strict';
+    huya_limit("7c90da14-da50-11ef-9c30-44fa662d354c");
+})();
