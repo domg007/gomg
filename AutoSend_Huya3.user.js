11ea680cd931ad14b985afb872888f3df5530a0a
domg007 <33490244+domg007@users.noreply.github.com>
2025-06-10 15:20:22 +0800

    Create AutoSend_Huya3.user.js

---
+++ AutoSend_Huya3.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         虎牙直播间自动跑房发弹幕
+// @namespace    https://item.taobao.com/item.htm?id=670749376549
+// @note         ↑是淘宝宝贝链接噢，欢迎进店定制脚本
+// @version      1.25.0620.01
+// @description  自动打开直播间发送指定弹幕后自动进入下个直播间【淘宝店铺（因稀有才珍贵）制作】_1.25.0620
+// @author       【淘宝店铺（因稀有才珍贵）】
+// @match        *://www.huya.com/*
+// @exclude      https://www.huya.com/g
+// @icon         https://a.msstatic.com/huya/main3/widget/list-tags/img/blueRay_tips_9d4b7.png
+// @require      https://fastly.jsdelivr.net/combine/gh/domg007/gomg@V1.2.0605.02/liveAutoSend8_Huya.js
+// @grant        GM_openInTab
+// @grant        GM_notification
+// @license      因稀有才珍贵
+// ==/UserScript==
+
+$(window).ready(() =>{
+	liveAutoSend("6e1d2cbfdcb6c7e3bbc8e71a3013d9ad");
+});
